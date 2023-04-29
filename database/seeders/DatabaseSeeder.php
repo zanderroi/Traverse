de574836d985f905da26043896a9f818322cdc17
Nel <jhonnelbaron03@gmail.com>
2023-04-29 18:10:12 +0800

    Admin Dashboard and Data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // \App\Models\Cars::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,7 +21,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/admin') }}">
                     Traverse
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -59,11 +59,90 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
 
-    <h2> WELCOME ADMIN </h2>
-</body>
+    <h2 class="text-center"> WELCOME ADMIN </h2>
+
+<div class="row">
+    <div class="col-md-4">
+        <div class="card bg-black text-white">
+            <div class="card-body">
+                <h5 class="card-title text-center">Total cars</h5>
+                <p class="card-text text-center">{{ $carsCount }}</p>
+                <hr>
+                <table class="table table-borderless text-white">
+                    <tr>
+                        <td>Booked</td>
+                        <td> 0 </td>
+                    </tr>
+                    <tr>
+                        <td>Available</td>
+                        <td> 5 </td>
+                    </tr>
+                </table>
+                <hr>
+                <div class="d-flex justify-content-center">
+                    <a href="#" class="btn btn-outline-light">
+                        View more details
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card bg-secondary text-white">
+            <div class="card-body">
+                <h5 class="card-title text-center">Total car owners</h5>
+                <p class="card-text text-center">{{ $carOwners }}</p>
+                <hr>
+                <table class="table table-borderless text-white">
+                    <tr>
+                        <td>On Transanctions</td>
+                        <td> 0 </td>
+                    </tr>
+                    <tr>
+                        <td>Vacant</td>
+                        <td> 5 </td>
+                    </tr>
+                </table>
+                <hr>
+                <div class="d-flex justify-content-center">
+                    <a href="#" class="btn btn-outline-light">
+                        View more details
+                    </a>
+                </div>
+                
+
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card bg-success text-white">
+            <div class="card-body">
+                <h5 class="card-title text-center">Total customers</h5>
+                <p class="card-text text-center">{{ $customers }}</p>
+                <hr>
+                <table class="table table-borderless text-white">
+                    <tr>
+                        <td>On Transactions</td>
+                        <td> 0 </td>
+                    </tr>
+                    <tr>
+                        <td>Free</td>
+                        <td> 3 </td>
+                    </tr>
+                </table>
+                <hr>
+                <div class="d-flex justify-content-center">
+                    <a href="#" class="btn btn-outline-light">
+                        View more details
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 </html>
+
