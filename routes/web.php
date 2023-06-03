dff891d8353c53f9a84505b950c00c9d0707f9b0
Zander Roi Tabelona <zandertabelona@gmail.com>
2023-06-03 11:43:18 +0800

    Route Update

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ use App\Models\Car;
 | be assigned to the "web" middleware group. Make something great!
 |
 // */
-//welcomenew
+
 Route::get('/', function () {
     $cars = Car::where('status', 'available')->get();
     return view('welcome', compact('cars'));
