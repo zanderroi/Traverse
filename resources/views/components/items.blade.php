486f68cd53018f0eccd44461ca9a4f1ec41d38d6
Zander Roi Tabelona <zandertabelona@gmail.com>
2023-06-05 17:12:19 +0800

    Navbar Update

--- resources/views/components/items.blade.php
+++ resources/views/components/items.blade.php
@@ -19,7 +19,7 @@
         
     @endif
 
-    <div class="sm:fixed sm:top-0 sm:right-0 text-right ml-6">
+    <div class="sm:fixed sm:top-0 sm:right-0 text-right">
 
             <a href="{{ route('contact') }}" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Contact Us! </a>
     </div>
