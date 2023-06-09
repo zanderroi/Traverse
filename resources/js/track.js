a84f898ba135c11cb5e7b2ac6b9cb83e8510e94b
Ric <rdgjr.pro@gmail.com>
2023-06-09 17:56:32 +0800

    Track merge

--- resources/js/track.js
+++ resources/js/track.js
@@ -1,63 +1,67 @@
-
-import 'bootstrap';
 import { Loader } from '@googlemaps/js-api-loader';
 import axios from 'axios';
 
 const loader = new Loader({
-    apiKey: 'AIzaSyCpEj-6GoyK9TIbNYMVijvzNh_DqRsC-84', // Replace with your actual API key
-    version: 'weekly',
-    libraries: ['places']
+  apiKey: 'AIzaSyCpEj-6GoyK9TIbNYMVijvzNh_DqRsC-84', // Replace with your actual API key
+  version: 'weekly',
+  libraries: ['places']
 });
 
-    let map;
-
+let map;
+let marker; // Global variable to store the marker instance
 
 loader.load().then(async () => {
-    const { Map } = await google.maps.importLibrary('maps');
+  const { Map } = await google.maps.importLibrary('maps');
 
-    const initialLatitude = parseFloat(document.getElementById('latitude').textContent);
-    const initialLongitude = parseFloat(document.getElementById('longitude').textContent);
+  const initialLatitude = parseFloat(document.getElementById('latitude').textContent);
+  const initialLongitude = parseFloat(document.getElementById('longitude').textContent);
 
-    map = new Map(document.getElementById('map'), {
-        center: { lat: initialLatitude, lng: initialLongitude },
-        zoom: 15
-    });
+  map = new Map(document.getElementById('map'), {
+    center: { lat: initialLatitude, lng: initialLongitude },
+    zoom: 15
+  });
 
-    // Call the updateLocation function initially
-    updateMapCenter(initialLatitude, initialLongitude);
+  // Call the updateLocation function initially
+  updateMapCenter(initialLatitude, initialLongitude);
 
-    // Set an interval to update the location every 10 seconds
-    setInterval(() => {
-        const latitude = parseFloat(document.getElementById('latitude').textContent);
-        const longitude = parseFloat(document.getElementById('longitude').textContent);
-        updateLocation();
-    }, 10000);
+  // Set an interval to update the location every 10 seconds
+  setInterval(updateLocation, 10000);
 });
 
-    // Get the car ID from the current URL
-    var carId = document.getElementById('carId').textContent;
+// Get the car ID from the current URL
+const carId = document.getElementById('carId').textContent;
 
 function updateLocation() {
-    axios
-        .get(`/api/car_owner/location/`+carId)
-        .then(response => {
-            document.getElementById('latitude').textContent = response.data.latitude;
-            document.getElementById('longitude').textContent = response.data.longitude;
+  axios
+    .get(`/api/car_owner/location/${carId}`)
+    .then(response => {
+      const latitude = parseFloat(response.data.latitude);
+      const longitude = parseFloat(response.data.longitude);
 
-            updateMapCenter(response.data.latitude, response.data.longitude);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+      document.getElementById('latitude').textContent = latitude;
+      document.getElementById('longitude').textContent = longitude;
+
+      updateMapCenter(latitude, longitude);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 function updateMapCenter(latitude, longitude) {
-    map.setCenter({ lat: latitude, lng: longitude });
-    new google.maps.Marker({
-        position: { lat: latitude, lng: longitude },
-        map: map,
-        title: 'Car Location'
-    });
+  map.setCenter({ lat: latitude, lng: longitude });
+
+  // Remove the existing marker if it exists
+  if (marker) {
+    marker.setMap(null);
+  }
+
+  // Create a new marker at the updated position
+  marker = new google.maps.Marker({
+    position: { lat: latitude, lng: longitude },
+    map: map,
+    title: 'Car Location'
+  });
 }
 
 window.axios = axios;
--- resources/views/car_owner/location.blade.php
+++ resources/views/car_owner/location.blade.php
@@ -7,7 +7,7 @@
             <h1 class="text-3xl font-bold pl-7 ml-4 mt-6 pt-4 mb-3 mr-5 text-white">Car Location</h1>
         </div>
       </div>
-    {{-- <p>Car ID: <span id="carId">{{ $carId }} </span></p> --}}
+    <p>Car ID: <span id="carId">{{ $carId }} </span></p>
     <p>Latitude: <span id="latitude">{{ $latitude }}</span></p>
     <p>Longitude: <span id="longitude">{{ $longitude }}</span></p>
     <div id="map" class="mx-auto" style="width:800px; height: 600px;"></div>
@@ -15,7 +15,7 @@
 
 @push('scripts')
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCpEj-6GoyK9TIbNYMVijvzNh_DqRsC-84"></script>
-    {{-- <script src="{{ asset('js/track.js') }}"></script> --}}
+    <script src="{{ asset('js/track.js') }}"></script>
 @endpush
 
 
